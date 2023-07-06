d7ae9913cbb033fd400b9d51f87b57d41238d399
Oskar Liew <oskar@liew.se>
2023-07-06 14:11:07 +0200

    Extract volume slider

--- .config/awesome/widget/volume/init.lua
+++ .config/awesome/widget/volume/init.lua
@@ -2,6 +2,7 @@ local wibox = require("wibox")
 local awful = require("awful")
 local gears = require("gears")
 local clickable_container = require("widget.clickable-container")
+local slider = require("widget.slider")
 local dpi = require("beautiful").xresources.apply_dpi
 local config_dir = gears.filesystem.get_configuration_dir()
 local widget_icon_dir = config_dir .. "widget/volume/icons/"
@@ -52,54 +53,8 @@ local function return_button()
 		preferred_positions = { "right", "left", "top", "bottom" },
 	})
 
-	local adjust_screen = awful.screen.focused()
-	local adjust_height = dpi(156)
-	local adjust_width = dpi(36)
-	local volume_adjust = wibox({
-		screen = adjust_screen,
-		x = adjust_screen.geometry.width - adjust_width - beautiful.useless_gap,
-		y = (adjust_height / 2),
-		width = adjust_width,
-		height = adjust_height,
-		shape = gears.shape.rounded_rect,
-		visible = false,
-		ontop = true,
-	})
-
-	local volume_bar = wibox.widget({
-		widget = wibox.widget.progressbar,
-		shape = gears.shape.rounded_rect,
-		color = beautiful.hud_slider_fg,
-		background_color = beautiful.hud_slider_bg,
-		max_value = 100,
-		value = 0,
-	})
-
-	volume_adjust:setup({
-		layout = wibox.layout.align.vertical,
-		{
-			wibox.container.margin(
-				volume_bar,
-				adjust_width * 0.25,
-				adjust_width * 0.40,
-				adjust_width * 0.40,
-				adjust_width * 0.40
-			),
-			forced_height = adjust_height * 0.80,
-			direction = "east",
-			layout = wibox.container.rotate,
-		},
-		wibox.container.margin(volume_imagebox, adjust_width * 0.25, adjust_width * 0.25),
-	})
-
-	-- create a 4 second timer to hide the volume adjust
-	-- component whenever the timer is started
-	local hide_volume_adjust = gears.timer({
-		timeout = 3,
-		autostart = true,
-		callback = function()
-			volume_adjust.visible = false
-		end,
+	local volume_slider = slider({
+		imagebox = volume_imagebox,
 	})
 
 	-- Functionality
@@ -115,8 +70,7 @@ local function return_button()
 					return
 				end
 
-				volume_widget.spacing = dpi(5)
-				volume_bar.value = volume_percentage
+				volume_slider.set_value(volume_percentage)
 
 				-- Update tooltip text
 				local tooltip_text = volume_percentage .. "%"
@@ -130,11 +84,7 @@ local function return_button()
 
 				if muted then
 					icon_name = icon_name .. "-mute"
-					volume_imagebox.icon:set_image(gears.surface.load_uncached(widget_icon_dir .. icon_name .. ".svg"))
-					return
-				end
-
-				if volume_percentage < 50 then
+				elseif volume_percentage < 50 then
 					icon_name = icon_name .. "-small"
 				else
 					icon_name = icon_name .. "-notice"
@@ -142,12 +92,7 @@ local function return_button()
 				volume_imagebox.icon:set_image(gears.surface.load_uncached(widget_icon_dir .. icon_name .. ".svg"))
 
 				-- make volume_adjust component visible
-				if volume_adjust.visible then
-					hide_volume_adjust:again()
-				else
-					volume_adjust.visible = true
-					hide_volume_adjust:start()
-				end
+				volume_slider.display()
 			end
 		)
 	end
